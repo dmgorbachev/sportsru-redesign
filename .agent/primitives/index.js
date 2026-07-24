6ec0cd0091409c25f6d815db2e19ecf920dd78a2
Dmitry <hermes@8574613-cn065576.twc1.net>
2026-07-24 09:43:31 +0000

    feat: add deslop agent rules + Sasha skill for interface texts

---
+++ .agent/primitives/index.js
@@ -0,0 +1,7 @@
+import "./colors.css";
+import "./layout.css";
+import "./typography.css";
+
+export { getIcon, icons } from "./icons";
+export { getColorToken } from "./colors";
+export * from "./tokens";
